826cca17a5e36a44a7d3ba2ccd77c1492c43888d
RogelioBenavides <77594197+RogelioBenavides@users.noreply.github.com>
2021-03-08 11:51:31 -0600

    Add files via upload

---
+++ Nombre.java
@@ -0,0 +1,16 @@
+//Rogelio Eduardo Benavides De La Rosa - 574161
+//El objetivo de este programa es desplegar mi nombre en pantalla
+import java.io.*;
+class Nombre {
+   public static void main(String[] args){
+      System.out.println("\t |||||||||||||||\t\t||||||||||||||\t\t||||||||||||||\t\t||||||||||||||\t\t|||||\t\t\t\t\t||||||||||||||\t\t||||||||||||||");
+      System.out.println("\t |||||\t\t|||||\t\t|||||\t\t|||||\t\t|||||\t\t|||||\t\t|||||\t\t\t\t\t|||||\t\t\t\t\t\t  ||||\t\t\t|||||\t\t|||||");
+      System.out.println("\t |||||||||||||||\t\t|||||\t\t|||||\t\t|||||\t\t\t\t\t||||||||||||||\t\t|||||\t\t\t\t\t\t  ||||\t\t\t|||||\t\t|||||");
+      System.out.println("\t ||||||||||\t\t\t\t|||||\t\t|||||\t\t|||||\t||||||||\t\t||||||||||||||\t\t|||||\t\t\t\t\t\t  ||||\t\t\t|||||\t\t|||||");
+      System.out.println("\t |||||||||||||||\t\t|||||\t\t|||||\t\t|||||\t\t|||||\t\t|||||\t\t\t\t\t|||||\t\t\t\t\t\t  ||||\t\t\t|||||\t\t|||||");
+      System.out.println("\t |||||\t\t|||||\t\t||||||||||||||\t\t||||||||||||||\t\t||||||||||||||\t\t||||||||||||||\t\t||||||||||||||\t\t||||||||||||||");
+   }
+}
+
+
+
---
+++ PrimerPrograma.java
@@ -0,0 +1,12 @@
+//Rogelio Eduardo Benavides De La Rosa - 574161
+//El objetivo de este programa es desplegar 4 enunciados sobre el curso de programación
+import java.io.*;
+class PrimerPrograma {
+   public static void main(String[] args){
+      System.out.println("\t Curso de Principios de Programacion\n");
+      System.out.println("\t \t \t \t \t \t1-Marzo\n");
+      System.out.println("****Rogelio Eduardo Benavides De La Rosa****\n");
+      System.out.println("\t \t \t Mi primer programa en Java\n");
+   }
+}
+
